e458291288f4f97966cce888a68453dc8b7cc268
Ratchanon Bualeesornsakun <ratchanon.blssk@gmail.com>
2024-09-05 19:32:58 +0700

    Lessons: Data Function | Interpolation & v-html | Method | Attribute Binding | Data Array | Data Object

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,122 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  data() {
+    return {
+      firstName: 'รัชชานนท์',
+      lastName: 'บัวลีสอนสกุล',
+      age: 27,
+      address: '<strong>ปทุมธานี</strong>',
+      picture: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
+      size: 150,
+      social: 'https://www.facebook.com/ratchanon.bua',
+      hobbies: ['การเล่นเกม', 'การฟังเพลง', 'การไลฟ์สตรีมสด'],
+      general: { gender: "ชาย", weight: 67.5, height: 175, status: false }
+    }
+  },
+  methods: {
+    getFullName() {
+      return `${this.firstName + ' ' + this.lastName}`
+    }
+  }
+}
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
-
-  <RouterView />
+  <RouterView /> -->
+  <section>
+    <img :src="picture" :width="size" :height="size" alt="Image" />
+    <h1>ชื่อ-นามสกุล: {{ getFullName() }} | อายุ: {{ age }} ปี</h1>
+    <p>ที่อยู่: <span v-html="address"></span></p>
+    <p>Social: <a :href="social" target="_blank">Facebook</a></p>
+    <p>งานอดิเรก:</p>
+    <ul>
+      <li>{{ hobbies[0] }}</li>
+      <li>{{ hobbies[1] }}</li>
+      <li>{{ hobbies[2] }}</li>
+    </ul>
+    <p>ข้อมูลพื้นฐาน:</p>
+    <ul>
+      <li>เพศ: {{ general.gender }}</li>
+      <li>น้ำหนัก: {{ general.weight }} กก.</li>
+      <li>ส่วนสูง: {{ general.height }} ซม.</li>
+      <li>โรคประจำตัว: {{ general.status }}</li>
+    </ul>
+  </section>
 </template>
 
 <style scoped>
-header {
+/* header {
   line-height: 1.5;
   max-height: 100vh;
-}
+} */
 
-.logo {
+/* .logo {
   display: block;
   margin: 0 auto 2rem;
-}
+} */
 
-nav {
+/* nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
   margin-top: 2rem;
-}
+} */
 
-nav a.router-link-exact-active {
+/* nav a.router-link-exact-active {
   color: var(--color-text);
-}
+} */
 
-nav a.router-link-exact-active:hover {
+/* nav a.router-link-exact-active:hover {
   background-color: transparent;
-}
+} */
 
-nav a {
+/* nav a {
   display: inline-block;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
-}
+} */
 
-nav a:first-of-type {
+/* nav a:first-of-type {
   border: 0;
-}
+} */
 
 @media (min-width: 1024px) {
-  header {
+  /* header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
+  } */
 
-  .logo {
+  /* .logo {
     margin: 0 2rem 0 0;
-  }
+  } */
 
-  header .wrapper {
+  /* header .wrapper {
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
-  }
+  } */
 
-  nav {
+  /* nav {
     text-align: left;
     margin-left: -1rem;
     font-size: 1rem;
 
     padding: 1rem 0;
     margin-top: 1rem;
-  }
+  } */
 }
 </style>
