1157c97b1b28eb9a545d9b45b73a8a4be1c0b420
Ratchanon Bualeesornsakun <ratchanon.blssk@gmail.com>
2024-09-05 11:25:43 +0700

    Install jQuery & Change Title Page with Router

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import $ from 'jquery'
+
+$(document).ready(function () {
+  console.log('Document Ready')
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
