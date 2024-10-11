ebcc45c64ea7dcf08a05eb494eed313c7c9b31d3
Ratchanon Bualeesornsakun <ratchanon.blssk@gmail.com>
2024-10-11 20:00:54 +0700

    Change Navigation Menu & Icons

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -2,34 +2,36 @@
 import { RouterLink } from "vue-router";
 
 import IconHome from "./icons/IconHome.vue";
+import IconFeed from "./icons/IconFeed.vue";
 import IconNotify from "./icons/IconNotify.vue";
-import IconSetting from "./icons/IconSetting.vue";
 import IconProfile from "./icons/IconProfile.vue";
 
 export default {
   name: "NavBarComponent",
-  components: { IconHome, IconNotify, IconSetting, IconProfile, RouterLink },
+  components: { IconHome, IconFeed, IconNotify, IconProfile, RouterLink },
+  data() {
+    return {
+      btnCSS: "inline-flex flex-col items-center justify-center px-4 hover:bg-gray-50 dark:hover:bg-gray-800 group",
+      navCSS: "w-6 h-6 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500",
+    };
+  },
 };
 </script>
 
 <template>
-  <nav class="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+  <nav class="fixed bottom-0 left-0 z-50 w-full h-12 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
     <div class="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
-      <RouterLink to="/" class="inline-flex flex-col items-center justify-center px-4 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-        <IconHome cssClass="w-6 h-6 mt-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" />
-        <span class="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Home</span>
+      <RouterLink :class="btnCSS" title="Home" to="/">
+        <IconHome :cssClass="navCSS" />
       </RouterLink>
-      <RouterLink to="/notifications" class="inline-flex flex-col items-center justify-center px-4 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-        <IconNotify cssClass="w-6 h-6 mt-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" />
-        <span class="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Notifications</span>
+      <RouterLink :class="btnCSS" title="Feed" to="/feeds">
+        <IconFeed :cssClass="navCSS" />
       </RouterLink>
-      <RouterLink to="/settings" class="inline-flex flex-col items-center justify-center px-4 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-        <IconSetting cssClass="w-6 h-6 mt-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" />
-        <span class="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Settings</span>
+      <RouterLink :class="btnCSS" title="Notifications" to="/notifications">
+        <IconNotify :cssClass="navCSS" />
       </RouterLink>
-      <RouterLink to="/profile" class="inline-flex flex-col items-center justify-center px-4 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-        <IconProfile cssClass="w-6 h-6 mt-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" />
-        <span class="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Profile</span>
+      <RouterLink :class="btnCSS" title="Profile" to="/profile">
+        <IconProfile :cssClass="navCSS" imageURL="https://images.pexels.com/photos/27897903/pexels-photo-27897903/free-photo-of-close-up-portrait-of-a-man.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
       </RouterLink>
     </div>
   </nav>
